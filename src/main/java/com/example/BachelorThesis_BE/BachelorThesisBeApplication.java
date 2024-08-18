f6cebf228bcf4cf5813c7a1d835a2a345e15548f
armandbecheru <100259426+armandbecheru@users.noreply.github.com>
2024-08-18 13:42:14 +0300

    Initial build for SpringBoot

---
+++ src/main/java/com/example/BachelorThesis_BE/BachelorThesisBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.BachelorThesis_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BachelorThesisBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BachelorThesisBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BachelorThesis_BE/BachelorThesisBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BachelorThesis_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BachelorThesisBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
